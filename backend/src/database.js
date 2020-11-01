25663b4eef7e4a5e202159cd8fa5affaffa8ea0b
DanielESanchez <47567586+DanielESanchez@users.noreply.github.com>
2020-11-01 15:02:30 -0600

    Update database.js

--- backend/src/database.js
+++ backend/src/database.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb+srv://Daniel:Paseo.2234@jalon-universitario.2jmqy.mongodb.net/jalon-universitario?retryWrites=true&w=majority', {
-//mongoose.connect('mongodb://localhost/jalon-universitario', {
+mongoose.connect('mongodb://localhost/jalon-universitario', {
     useCreateIndex: true,
     useNewUrlParser: true,
     useUnifiedTopology: true
